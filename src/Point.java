2cf1baba07c8236c85dbed8cc6ea3dcb85831d98
Quazi <quazih2789@nycstudents.net>
2023-09-28 22:03:07 -0400

    Errors fixed on Point class

--- src/Point.java
+++ src/Point.java
@@ -2,6 +2,12 @@ public class Point {
     private int x;
     private int y;
 
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
     public Point(int x) {
         this.x = x;
         y = x;
@@ -25,7 +31,7 @@ public class Point {
         } else if (x < 0 && y < 0) {
             return "III";
         } else if (x > 0 && y < 0) {
-            return "VI";
+            return "IV";
         } else if (x == 0 && y == 0) {
             return "origin";
         } else if ((x == 0 && y < 0) || (x == 0 && y > 0)) {
@@ -43,6 +49,14 @@ public class Point {
 
     public int getX() {
         return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
     }
 
 }
